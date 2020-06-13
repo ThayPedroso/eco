4d2f22b4b9c038a86e7c8f30f6f8becfb6d7eb09
ThayPedroso <44008476+ThayPedroso@users.noreply.github.com>
2020-06-13 17:42:49 -0300

    basicOnRoutes
    request param, query param and request body.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,46 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users', (req, res, next) => {
-    console.log('Listagem de usuários')
+app.use(express.json())
 
-    res.json([
-        'Thayla',
-        'Lívia',
-        'Cezar'
-    ])
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+]
+
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parâmetros para criação/atualização de informações
+
+app.get('/users', (req, res, next) => {
+    const search = String(req.query.search)
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
+
+    return res.json(filteredUsers)
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+
+    const user = users[id]
+
+    return res.json(user)
+})
+
+app.post('/users', (req, res) => {
+    const data = req.body
+
+    console.log(data)
+
+    const user = {
+        name: data.name,
+        email: data.email
+    }
+
+    return res.json(user)
 })
 
 app.listen(3333)
